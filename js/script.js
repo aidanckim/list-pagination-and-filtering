6109dfb245b24c0ea97495130094cf4ddce8c327
Christopher Aidan Kim <christopheraidank@gmail.com>
2020-06-07 23:12:55 -0700

    Improved list pagination and filtering
    Added
    - create search bar
    - filter student list and match user input
    - pagination links
    - 'no results' message when search does not match

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ GLOBAL VARIABLES
 
 const studentList = document.querySelectorAll('.student-item')
 const numOfStudents = 10
+const div = document.createElement('div')
+const page = document.querySelector('.page')
 
 /***************
 DISPLAY STUDENTS
@@ -30,8 +32,6 @@ PAGINATION LINKS
 
 const appendPageLinks = (list) => {
   const numOfPages = Math.ceil(list.length / numOfStudents)
-  const div = document.createElement('div')
-  const page = document.querySelector('.page')
   const ul = document.createElement('ul')
   div.className = 'pagination'
   page.appendChild(div)
@@ -52,7 +52,6 @@ const appendPageLinks = (list) => {
 
     a.addEventListener('click', (e) => {
       const link = document.querySelectorAll('a')
-
       for (let i = 0; i < link.length; i++) {
         link[i].className = 'none'
       }
@@ -63,3 +62,90 @@ const appendPageLinks = (list) => {
 }
 
 appendPageLinks(studentList)
+
+/*********
+SEARCH BAR
+**********/
+
+const searchDiv = document.createElement('div')
+const input = document.createElement('input')
+const button = document.createElement('button')
+const header = document.querySelector('.page-header')
+searchDiv.className = 'student-search'
+input.placeholder = 'Search for students...'
+button.textContent = 'Search'
+
+searchDiv.appendChild(input)
+searchDiv.appendChild(button)
+header.appendChild(searchDiv)
+
+/*********
+NO RESULTS
+**********/
+
+const noResults = document.createElement('h2')
+noResults.textContent = 'No results found... search again!'
+page.appendChild(noResults)
+noResults.style.display = 'none'
+
+/*********************
+FUNCTIONING SEARCH BAR
+**********************/
+
+function searchBar () {
+  const userSearch = document.querySelector('input')
+  const filter = userSearch.value.toUpperCase()
+  for (let i = 0; i < studentList.length; i++) {
+    const aLink = studentList[i].getElementsByTagName('h3')[0]
+    const txtValue = aLink.textContent || aLink.innerText
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      studentList[i].style.display = ''
+    } else {
+      studentList[i].style.display = 'none'
+    }
+  }
+}
+
+/**********************
+PAGINATE SEARCH RESULTS
+***********************/
+
+function paginateSearch () {
+  const array = []
+  for (let i = 0; i < studentList.length; i++) {
+    if (
+      input.value.length !== 0 &&
+      studentList[i]
+        .querySelector('h3')
+        .textContent.toLowerCase()
+        .includes(input.value.toLowerCase())
+    ) {
+      array.push(studentList[i])
+    }
+  }
+  if (array.length === 0) {
+    noResults.style.display = ''
+  } else {
+    noResults.style.display = 'none'
+  }
+  div.innerHTML = ''
+  showPage(array, 1)
+  appendPageLinks(array)
+}
+
+/*************
+EVENT LISTENER
+**************/
+
+input.addEventListener('keyup', (e) => {
+  e.preventDefault()
+
+  if (input.value !== '') {
+    searchBar()
+    paginateSearch()
+  } else {
+    div.innerHTML = ''
+    showPage(studentList, 1)
+    appendPageLinks(studentList)
+  }
+})
